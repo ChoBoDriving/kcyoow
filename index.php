b838d76fc424031ac9ea12395b7ce0c5749ee709
김철연 <kcy021012@gmail.com>
2021-05-21 16:04:56 +0000

    test

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta http-equiv='refresh' content="0; url=https://junctionxkcy-jvccl.run.goorm.io/homepage.php">
+
+
+    <title>redirecting</title>
+  </head>
+  <body>
+
+  </body>
+</html>
