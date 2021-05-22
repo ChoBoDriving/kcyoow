67fb76d15ea669616cb123b5405f6f31483408e6
김철연 <kcy021012@gmail.com>
2021-05-22 10:17:55 +0000

    first update

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <!-- Required meta tags -->
-    <meta http-equiv='refresh' content="0; url=https://junctionxkcy-jvccl.run.goorm.io/homepage.php">
+    <meta http-equiv='refresh' content="0; url=https://junctionxkcy-jvccl.run.goorm.io/homepage.html">
 
 
     <title>redirecting</title>
